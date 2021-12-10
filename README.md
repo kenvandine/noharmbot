1f08e11f597e41b11c30e1e4b896e83518f28db8
Ken VanDine <ken@vandine.org>
2021-12-10 10:00:06 -0500

    Initial project

---
+++ README.md
@@ -0,0 +1,13 @@
+# NoHarm discord bot
+
+Install necessary dependencies, discord and dadjokes python libraries are required
+
+`
+pip install discord dadjokes
+`
+
+To run:
+
+`
+NOHARM_TOKEN=<TOKEN> python3 noharm.py
+`
---
+++ noharm.py
@@ -0,0 +1,40 @@
+import os
+import discord
+from dadjokes import Dadjoke
+
+client = discord.Client()
+
+usage = '''\
+Welcome to the NoHarm bot!
+
+Available commands:
+    $hello 
+    $dadjoke
+'''.format(length='multi-line', ordinal='second')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$dadjoke'):
+        dadjoke = Dadjoke()
+        await message.channel.send(dadjoke.joke)
+        print(dadjoke.joke)
+        dadjoke = None
+    if message.content.startswith('$help'):
+        await message.channel.send(usage)
+
+token = os.getenv('NOHARM_TOKEN')
+if token:
+  client.run(token)
+else:
+  print("NOHARM_TOKEN environment variable required")
+
