3b865a7abf7c60947e9414d31e0399bbc9dc2079
Drew VanDine <drew@vandine.org>
2021-12-10 21:37:34 -0500

    precede commands with a hyphon

--- noharm.py
+++ noharm.py
@@ -8,8 +8,8 @@ usage = '''\
 Welcome to the NoHarm bot!
 
 Available commands:
-    $hello 
-    $dadjoke
+    -hello 
+    -dadjoke
 '''.format(length='multi-line', ordinal='second')
 
 @client.event
@@ -21,16 +21,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('-hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('$dadjoke'):
+    if message.content.startswith('-dadjoke'):
         dadjoke = Dadjoke()
         await message.channel.send(dadjoke.joke)
         print(dadjoke.joke)
         dadjoke = None
-    if message.content.startswith('$help'):
+    if message.content.startswith('-help'):
         await message.channel.send(usage)
+
+    
 
 token = os.getenv('NOHARM_TOKEN')
 if token:
